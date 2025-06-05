c642ccd2413dcd9200d619bb57af2b0d53a212f6
jk369 <jk369.park@gmail.com>
2025-06-05 16:29:37 +0900

    branch commit

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -3,16 +3,32 @@
  */
 public class DeviceDriver {
 
+    FlashMemoryDevice hw;
+
     public DeviceDriver(FlashMemoryDevice hardware) {
         // TODO: implement this method
+        hw = hardware;
     }
 
     public byte read(long address) {
-        // TODO: implement this method
-        return -1;
+        byte returnData = 0;
+        returnData = hw.read(address);
+        for (int i = 0; i < 4; i++) {
+            if (returnData != hw.read(address)) {
+                throw new ReadFailException("Read failed at address: " + address);
+            }
+        }
+        return returnData;
     }
 
     public void write(long address, byte data) {
         // TODO: implement this method
+        hw.write(address, data);
+    }
+}
+
+class ReadFailException extends RuntimeException {
+    public ReadFailException(String message) {
+        super(message);
     }
 }
--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,14 +1,55 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
+    @Mock
+    FlashMemoryDevice hardware;
 
-    @Test
+    /*@Test
     public void read_From_Hardware() {
         // TODO: replace hardware with a Test Double
         FlashMemoryDevice hardware = null;
         DeviceDriver driver = new DeviceDriver(hardware);
         byte data = driver.read(0xFF);
         assertEquals(0, data);
+    }*/
+
+    @Test
+    public void read_From_Hardware_equal() {
+        when(hardware.read(0xFF)).thenReturn((byte) 42);
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+        byte data = driver.read(0xFF);
+
+        assertEquals(42, data);
+        verify(hardware, times(5)).read(0xFF);
+    }
+
+    @Test
+    public void read_From_Hardware_exception() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte) 1, (byte) 1, (byte) 2, (byte) 1, (byte) 1);
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+
+        assertThrows(ReadFailException.class, () -> driver.read(0xFF));
+        //verify(hardware, times(5)).read(0xFF);
+    }
+
+    @Test
+    public void write_From_Hardware_normal() {
+        when(hardware.read(0xFF))
+                .thenReturn(null);
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+
+
+         //verify(hardware, times(5)).read(0xFF);
     }
 }
