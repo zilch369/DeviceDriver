08fb102bad1cabe8238999935f8ab24517ebea1a
jk369 <jk369.park@gmail.com>
2025-06-05 17:16:15 +0900

    branch commit

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -13,16 +13,22 @@ public class DeviceDriver {
     public byte read(long address) {
         byte returnData = 0;
         returnData = hw.read(address);
+        postConditionOfRead(address, returnData);
+        return returnData;
+    }
+
+    private void postConditionOfRead(long address, byte returnData) {
         for (int i = 0; i < 4; i++) {
             if (returnData != hw.read(address)) {
                 throw new ReadFailException("Read failed at address: " + address);
             }
         }
-        return returnData;
     }
 
     public void write(long address, byte data) {
-        // TODO: implement this method
+        if (hw.read(address) != (byte) 0xFF) {;
+            throw new IllegalStateException("Cannot write to address " + address + " as it is not empty.");
+        }
         hw.write(address, data);
     }
 }
@@ -30,5 +36,27 @@ public class DeviceDriver {
 class ReadFailException extends RuntimeException {
     public ReadFailException(String message) {
         super(message);
+    }
+}
+
+class App {
+
+    private DeviceDriver driver;
+
+    public App(DeviceDriver driver) {
+        this.driver = driver;
+    }
+
+    void readAndPrint(long addressFrom, long addressTo){
+        for (long address = addressFrom; address <= addressTo; address++) {
+            byte data = driver.read(address);
+            System.out.println("Address: " + address + ", Data: " + data);
+        }
+    }
+
+    void writeAll(byte data){
+        for (long address = 0x00; address <= 0x03; address++) {
+            driver.write(address, data);
+        }
     }
 }
--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -43,13 +43,55 @@ public class DeviceDriverTest {
     }
 
     @Test
-    public void write_From_Hardware_normal() {
-        when(hardware.read(0xFF))
-                .thenReturn(null);
+    public void write_From_Hardware_Normal() {
+        when(hardware.read(0xAA))
+                .thenReturn((byte) 0xFF);
 
         DeviceDriver driver = new DeviceDriver(hardware);
 
+        driver.write(0xAA, (byte) 0x55);
 
-         //verify(hardware, times(5)).read(0xFF);
+        verify(hardware).read(0xAA);
+        verify(hardware).write(0xAA, (byte) 0x55);
+    }
+
+    @Test
+    public void write_From_Hardware_Exception() {
+        when(hardware.read(0xAA))
+                .thenReturn((byte) 0x11);
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+
+        assertThrows(IllegalStateException.class, () -> driver.write(0xAA, (byte) 0x55));
+        verify(hardware).read(0xAA);
+        verify(hardware, never()).write(anyLong(), anyByte());
+    }
+
+    @Test
+    public void test_readAndPrint() {
+        // Assuming App class is implemented with readAndPrint method
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+        App app = new App(driver);
+
+        app.readAndPrint(0x00, 0x03);
+
+        // Verify that the read method was called for each address
+        verify(hardware, times(20)).read(anyLong());
+    }
+
+    @Test
+    public void writeAll() {
+        // Assuming App class is implemented with readAndPrint method
+        when(hardware.read(0xAA))
+                .thenReturn((byte) 0x11);
+
+        DeviceDriver driver = new DeviceDriver(hardware);
+        App app = new App(driver);
+
+        app.writeAll((byte) 0x11);
+
+        // Verify that the read method was called for each address
+        //verify(hardware, times(20)).read(anyLong());
     }
 }
